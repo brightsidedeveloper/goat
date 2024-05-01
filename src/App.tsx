abcf0d6bd01dc997e88d777c51dc6a7eecaa7516
Tim <tim@brightsidedeveloper.com>
2024-04-30 23:20:04 -0500

    add timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import clsx from 'clsx'
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 export default function App() {
   const [state, setState] = useState(false)
+  const [time, setTime] = useState(0)
 
   return (
-    <div className='group'>
-      GOAT STATE
-      <button onClick={() => setState(c => !c)}>Toggle</button>
+    <div>
+      GOAT STATE - {time < 10000 && time} -{' '}
+      <button
+        onClick={() => {
+          setState(!state)
+          setTime(Date.now())
+        }}
+      >
+        Toggle
+      </button>
       {[...new Array(50000)].map((_, i) => (
-        <Ez key={i} state={state} />
+        <Ez key={i} state={state} setTime={setTime} last={i === 50000 - 1} />
       ))}
     </div>
   )
 }
 
-function Ez({ state }: { state: boolean }) {
+function Ez({ state, last, setTime }: { state: boolean; last: boolean; setTime: Dispatch<SetStateAction<number>> }) {
+  useEffect(() => {
+    if (!last) return
+    setTime(c => Date.now() - c)
+  }, [state, last, setTime])
   return <div className={clsx(state ? 'bg-red-500' : 'bg-blue-500')}>Ez</div>
 }
