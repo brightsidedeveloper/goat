51d925bb9c4326a38e402be90bd0bf49742389fa
tim vanlerberg <tim@brightsidedeveloper.com>
2025-03-16 16:36:00 -0500

    EventCB

--- goat.go
+++ goat.go
@@ -10,6 +10,9 @@ func Alert(msg string) {
 	js.Global().Call("alert", msg)
 }
 
-func Fn(f func(this js.Value, args []js.Value) any) js.Func {
-	return js.FuncOf(f)
+func EventCB(f func(el js.Value, event js.Value)) js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		f(this, args[0])
+		return nil
+	})
 }
