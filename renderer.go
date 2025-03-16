51219ad686d8936cf90223db019bf359af19239b
tim vanlerberg <tim@brightsidedeveloper.com>
2025-03-16 11:09:19 -0500

    OMG

--- renderer.go
+++ renderer.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Renderer struct {
-	id       string
-	vdom     VNode
 	instance *ComponentInstance
 	comp     Component
 	props    any
+	id       string
+	vdom     VNode
 	mu       sync.Mutex
 }
 
@@ -46,15 +46,98 @@ func (r *Renderer) Render() {
 func (r *Renderer) updateDOM(newVdom VNode) {
 	doc := js.Global().Get("document")
 	container := doc.Call("getElementById", r.id)
-	if !container.Truthy() {
-		// Initial render: create the element
-		container = doc.Call("createElement", "div")
-		container.Set("id", r.id)
-		doc.Get("body").Call("appendChild", container)
+	if !r.vdom.DOMNode.Truthy() {
+		// Initial render
+		r.vdom = createDOM(newVdom, doc)
+		container.Call("appendChild", r.vdom.DOMNode)
+	} else {
+		// Diff and patch
+		diffAndPatch(r.vdom, newVdom, r.vdom.DOMNode, doc)
 	}
-	// For now, just set innerHTML (no diffing yet)
-	html := vdomToHTML(newVdom)
-	container.Set("innerHTML", html)
+}
+
+func createDOM(v VNode, doc js.Value) VNode {
+	var node js.Value
+	if v.Tag == "" {
+		node = doc.Call("createTextNode", v.Text)
+	} else {
+		node = doc.Call("createElement", v.Tag)
+		for key, value := range v.Attrs {
+			node.Call("setAttribute", key, value)
+		}
+		for event, handler := range v.Events {
+			node.Call("addEventListener", event, js.FuncOf(handler))
+		}
+		children := make([]VNode, len(v.Children))
+		for i, child := range v.Children {
+			childNode := createDOM(child, doc)
+			node.Call("appendChild", childNode.DOMNode)
+			children[i] = childNode
+		}
+		v.Children = children
+		if v.Text != "" {
+			node.Set("textContent", v.Text)
+		}
+	}
+	v.DOMNode = node
+	return v
+}
+
+func diffAndPatch(oldVdom, newVdom VNode, parent js.Value, doc js.Value) {
+	if oldVdom.Tag != newVdom.Tag || oldVdom.Text != newVdom.Text {
+		// Replace node if tag or text differs
+		newNode := createDOM(newVdom, doc)
+		parent.Call("replaceChild", newNode.DOMNode, oldVdom.DOMNode)
+		oldVdom.DOMNode = newNode.DOMNode
+		return
+	}
+
+	// Update attributes
+	for key, value := range newVdom.Attrs {
+		if oldVdom.Attrs[key] != value {
+			oldVdom.DOMNode.Call("setAttribute", key, value)
+		}
+	}
+	for key := range oldVdom.Attrs {
+		if _, exists := newVdom.Attrs[key]; !exists {
+			oldVdom.DOMNode.Call("removeAttribute", key)
+		}
+	}
+
+	// Update Events
+	for event, handler := range newVdom.Events {
+		if oldHandler, exists := oldVdom.Events[event]; exists {
+			oldVdom.DOMNode.Call("removeEventListener", event, js.FuncOf(oldHandler))
+		}
+		oldVdom.DOMNode.Call("addEventListener", event, js.FuncOf(handler))
+	}
+	for event := range oldVdom.Events {
+		if _, exists := newVdom.Events[event]; !exists {
+			oldVdom.DOMNode.Call("removeEventListener", event, js.FuncOf(oldVdom.Events[event]))
+		}
+	}
+
+	// Update children
+	maxLen := len(oldVdom.Children)
+	if len(newVdom.Children) > maxLen {
+		maxLen = len(newVdom.Children)
+	}
+	for i := 0; i < maxLen; i++ {
+		if i >= len(oldVdom.Children) {
+			// Add new child
+			newChild := createDOM(newVdom.Children[i], doc)
+			oldVdom.DOMNode.Call("appendChild", newChild.DOMNode)
+			oldVdom.Children = append(oldVdom.Children, newChild)
+		} else if i >= len(newVdom.Children) {
+			// Remove excess child
+			oldVdom.DOMNode.Call("removeChild", oldVdom.Children[i].DOMNode)
+			oldVdom.Children = oldVdom.Children[:i]
+			break
+		} else {
+			// Diff existing child
+			diffAndPatch(oldVdom.Children[i], newVdom.Children[i], oldVdom.DOMNode, doc)
+		}
+	}
 }
 
 func vdomToHTML(v VNode) string {
--- virtualdom.go
+++ virtualdom.go
@@ -5,16 +5,17 @@ import "syscall/js"
 type VNode struct {
 	Tag      string
 	Attrs    map[string]string
+	Events   map[string]func(js.Value, []js.Value) any // Event handlers
 	Children []VNode
 	Text     string
 	DOMNode  js.Value
-	// TODO: Need event listeners and complex props
 }
 
-func NewVNode(tag string, attrs map[string]string, children []VNode, text string) VNode {
+func NewVNode(tag string, attrs map[string]string, events map[string]func(js.Value, []js.Value) any, children []VNode, text string) VNode {
 	return VNode{
 		Tag:      tag,
 		Attrs:    attrs,
+		Events:   events,
 		Children: children,
 		Text:     text,
 	}
